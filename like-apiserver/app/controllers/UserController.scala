864488e84b4ac46f14a8d0a4282e3a932de43df9
Guan Guan <guanguan1114@gmail.com>
2015-06-16 21:23:12 +0800

    Update V1 APIs

--- like-apiserver/app/controllers/UserController.scala
+++ like-apiserver/app/controllers/UserController.scala
@@ -9,7 +9,6 @@ import services.{ PostService, UserService }
 import utils.QiniuUtil
 
 import scala.concurrent.Future
-import scala.concurrent.duration._
 
 /**
  * Created by Guan Guan
@@ -20,7 +19,6 @@ class UserController @Inject() (
     userService: UserService, postService: PostService) extends BaseController {
 
   def getInfo(id: Long) = UserAwareAction.async { implicit request =>
-
     userService.findById(id).flatMap {
       case Some(user) =>
         for {
@@ -29,83 +27,49 @@ class UserController @Inject() (
           countPosts <- postService.countByUserId(id)
           isFollowing <- if (request.userId.isDefined) userService.isFollowing(request.userId.get, id) else Future.successful(0)
         } yield {
-          Ok(Json.obj(
-            "code" -> 1,
-            "message" -> "Record(s) Found",
-            "data" -> Json.obj(
-              "user_id" -> id.toString,
-              "nickname" -> user.nickname,
-              "avatar" -> QiniuUtil.getAvatar(user.avatar, "large"),
-              "origin_avatar" -> QiniuUtil.getAvatar(user.avatar, "origin"),
-              "cover" -> QiniuUtil.getSizedImage(user.cover, getScreenWidth),
-              "likes" -> user.likes,
-              "is_following" -> isFollowing,
-              "count" -> Json.obj(
-                "post" -> countPosts,
-                "follow" -> countFriends,
-                "fan" -> countFriends
-              )
+          success(Messages("success.recordFound"), Json.obj(
+            "user_id" -> id.toString,
+            "nickname" -> user.nickname,
+            "avatar" -> QiniuUtil.getAvatar(user.avatar, "large"),
+            "origin_avatar" -> QiniuUtil.getAvatar(user.avatar, "origin"),
+            "cover" -> QiniuUtil.getSizedImage(user.cover, getScreenWidth),
+            "likes" -> user.likes,
+            "is_following" -> isFollowing,
+            "count" -> Json.obj(
+              "post" -> countPosts,
+              "follow" -> countFriends,
+              "fan" -> countFriends
             )
           ))
         }
-      case None => Future.successful(NotFound(Json.obj(
-        "code" -> 4014,
-        "field" -> "id",
-        "message" -> Messages("invalid.userId")
-      )))
+      case None => Future.successful(error(4014, Messages("invalid.userId")))
     }
   }
 
-  def updateNickname = SecuredAction.async(parse.json) { implicit request =>
+  def updateNickname() = SecuredAction.async(parse.json) { implicit request =>
     (request.body \ "nickname").asOpt[String] match {
       case Some(nickname) =>
-        userService.nicknameExists(nickname).flatMap {
-          case true => Future.successful(BadRequest(Json.obj(
-            "code" -> 4040,
-            "field" -> "nickname",
-            "message" -> Messages("invalid.nicknameExist")
-          )))
+        userService.nicknameExists(nickname.trim).flatMap {
+          case true => Future.successful(error(4040, Messages("invalid.nicknameExist")))
           case false =>
-            if (nickname.length > 20) {
-              Future.successful(BadRequest(Json.obj(
-                "code" -> 4040,
-                "field" -> "nickname",
-                "message" -> Messages("invalid.nicknameLong")
-              )))
-            } else if (nickname.length < 2) {
-              Future.successful(BadRequest(Json.obj(
-                "code" -> 4040,
-                "field" -> "nickname",
-                "message" -> Messages("invalid.nicknameLong")
-              )))
+            if (nickname.trim.length > 20) {
+              Future.successful(error(4040, Messages("invalid.nicknameLong")))
+            } else if (nickname.trim.length < 2) {
+              Future.successful(error(4040, Messages("invalid.nicknameShort")))
             } else {
-              userService.updateNickname(request.userId, nickname).map(_ => Ok(Json.obj(
-                "code" -> 1,
-                "message" -> "Change Nickname Success"
-              )))
+              userService.updateNickname(request.userId, nickname.trim).map(_ => success(Messages("success.nickname")))
             }
         }
-      case None => Future.successful(BadRequest(Json.obj(
-        "code" -> 4040,
-        "field" -> "nickname",
-        "message" -> Messages("invalid.nickname")
-      )))
+      case None => Future.successful(error(4040, Messages("invalid.nickname")))
     }
   }
 
-  def updateAvatar = SecuredAction.async(parse.json) { implicit request =>
+  def updateAvatar() = SecuredAction.async(parse.json) { implicit request =>
     (request.body \ "avatar").asOpt[String] match {
-      case Some(avatar) => {
-        userService.updateAvatar(request.userId, avatar).map(_ => Ok(Json.obj(
-          "code" -> 1,
-          "message" -> Messages("success.avatar")
-        )))
-      }
-      case None => Future.successful(BadRequest(Json.obj(
-        "code" -> 4041,
-        "field" -> "avatar",
-        "message" -> Messages("invalid.avatar")
-      )))
+      case Some(avatar) =>
+        userService.updateAvatar(request.userId, avatar).map(_ => success(Messages("success.avatar")))
+      case None =>
+        Future.successful(error(4041, Messages("invalid.avatar")))
     }
   }
 

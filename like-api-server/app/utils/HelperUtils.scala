64e5f9fea31c36a389f75d0183ce3e046a3ab465
Guan Guan <guanguan1114@gmail.com>
2015-07-01 16:14:36 +0800

    Add implicit convertion helper for redis

--- like-api-server/app/utils/HelperUtils.scala
+++ like-api-server/app/utils/HelperUtils.scala
@@ -1,10 +1,16 @@
 package utils
 
+import scala.language.implicitConversions
+
 /**
  * Created by Guan Guan
  * Date: 6/29/15
  */
 object HelperUtils {
+
+  implicit def long2String(value: Long): String = value.toString
+
+  implicit def int2String(value: Int): String = value.toString
 
   def parseTimestamp(timestamp: Option[String]): Seq[Option[Long]] = {
     if (timestamp.isDefined) {
@@ -13,7 +19,7 @@ object HelperUtils {
         t => try { Some(t.toLong) } catch { case _: Throwable => None }
       }
       // Replace None with a minimum timestamp in the array, e.g 1435190198,,1435185748 => 1435190198,1435185748,1435185748
-      if (tsArr.exists(_ != None)) {
+      if (tsArr.exists(_.isDefined)) {
         val min = tsArr.minBy(_.getOrElse(Long.MaxValue))
         tsArr.map(t => if (t.isDefined) t else min)
       } else {

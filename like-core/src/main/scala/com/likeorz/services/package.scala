804056f58b12352a586378324692cf0a6549def7
Guan Guan <guanguan1114@gmail.com>
2015-09-16 20:26:29 +0800

    Helper function to trace future elapsed time

---
+++ like-core/src/main/scala/com/likeorz/services/package.scala
@@ -0,0 +1,26 @@
+package com.likeorz
+
+import scala.concurrent._
+import scala.util.{ Success, Failure }
+
+package object services {
+
+  private val tZero = System.currentTimeMillis()
+
+  def printlnC(s: Any): Unit = println(Console.GREEN + s + Console.RESET)
+  def printlnE(s: Any): Unit = println(Console.RED + s + Console.RESET)
+
+  def timedFuture[T](name: String)(f: Future[T])(implicit ec: ExecutionContext): Future[T] = {
+    val start = System.currentTimeMillis()
+    printlnC(s"--> started [$name] at t0 + ${start - tZero}")
+    f.andThen {
+      case Success(t) =>
+        val end = System.currentTimeMillis()
+        printlnC(s"\t<-- finished [$name] after ${end - start} millis")
+      case Failure(ex) =>
+        val end = System.currentTimeMillis()
+        printlnE(s"\t<X> failed [$name], total time elapsed: ${end - start}\n$ex")
+    }
+  }
+
+}

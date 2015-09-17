c043b9746259a9e8434ff2cf741baa91ed8ff788
Guan Guan <guanguan1114@gmail.com>
2015-09-17 20:26:38 +0800

    Remove "ansj_seg dependency

--- like-api-server/app/utils/NlpUtils.scala
+++ like-api-server/app/utils/NlpUtils.scala
@@ -1,6 +1,5 @@
 package utils
 
-import org.ansj.splitWord.analysis.BaseAnalysis
 import play.api.Play
 import scala.collection.JavaConversions._
 
@@ -9,10 +8,6 @@ object NlpUtils {
   val sensitiveWord = SensitiveWord.getInstance()
 
   val illegalWords = Play.current.configuration.getStringList("tag.illegal-words").get.toList
-
-  def tokenize(str: String): Seq[String] = {
-    BaseAnalysis.parse(str).map(t => t.getName).toSeq
-  }
 
   def isContainSensitiveWord(word: String): Boolean = {
     if (illegalWords.exists(w => word.contains(w))) {

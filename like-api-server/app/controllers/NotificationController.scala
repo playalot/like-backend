bc196aeaf091e0a2e46d93e1be7738a5eba59b39
Guan Guan <guanguan1114@gmail.com>
2015-09-18 22:14:31 +0800

    API notificationV2, merge multiple likes

--- like-api-server/app/controllers/NotificationController.scala
+++ like-api-server/app/controllers/NotificationController.scala
@@ -3,7 +3,7 @@ package controllers
 import javax.inject.Inject
 
 import play.api.i18n.{ Messages, MessagesApi }
-import play.api.libs.json.Json
+import play.api.libs.json.{ JsObject, Json }
 import play.api.libs.concurrent.Execution.Implicits._
 import com.likeorz.services.NotificationService
 import utils.QiniuUtil
@@ -74,6 +74,69 @@ class NotificationController @Inject() (
       }
       success(Messages("success.found"), Json.obj(
         "notifications" -> Json.toJson(json),
+        "ts" -> ts
+      ))
+    }
+  }
+
+  def getNotificationsV2(ts: Option[Long] = None) = SecuredAction.async { implicit request =>
+
+    notificationService.getNotifications(request.userId, ts).map { results =>
+      val ts = results.lastOption.map(_._1.updated)
+
+      val combinedResults = results.foldLeft(scala.collection.mutable.MutableList[JsObject]()) { (b, a) =>
+        val (notification, user, postOpt) = a
+        if (notification.`type` == "FOLLOW") {
+          b += Json.obj(
+            "type" -> notification.`type`,
+            "user" -> Json.obj(
+              "user_id" -> user.id.toString,
+              "nickname" -> user.nickname,
+              "avatar" -> QiniuUtil.getAvatar(user.avatar, "small"),
+              "likes" -> user.likes
+            ),
+            "timestamp" -> notification.updated
+          )
+        } else {
+          if (notification.`type` == "LIKE"
+            && b.nonEmpty
+            && (b.last \ "type").as[String].contains("LIKE")
+            && notification.postId.get == (b.last \ "post" \ "post_id").as[Long]) {
+
+            val mergedJson = b.last.deepMerge(Json.obj(
+              "tags" -> (b.last \ "tags").as[List[String]].+:(notification.tagName.get)
+            ))
+            b.update(b.length - 1, mergedJson)
+
+          } else {
+            b += Json.obj(
+              "type" -> notification.`type`,
+              "user" -> Json.obj(
+                "user_id" -> user.id.toString,
+                "nickname" -> user.nickname,
+                "avatar" -> QiniuUtil.getAvatar(user.avatar, "small"),
+                "likes" -> user.likes
+              ),
+              "post" -> postOpt.map(postAndUser => Json.obj(
+                "post_id" -> postAndUser._1.id.get,
+                "content" -> QiniuUtil.getPhoto(postAndUser._1.content, "small"),
+                "user" -> Json.obj(
+                  "user_id" -> postAndUser._2.id.toString,
+                  "nickname" -> postAndUser._2.nickname,
+                  "avatar" -> QiniuUtil.getAvatar(postAndUser._2.avatar, "small"),
+                  "likes" -> postAndUser._2.likes
+                )
+              )),
+              "tags" -> notification.tagName.toSeq,
+              "timestamp" -> notification.updated
+            )
+          }
+        }
+        b
+      }
+
+      success(Messages("success.found"), Json.obj(
+        "notifications" -> Json.toJson(combinedResults),
         "ts" -> ts
       ))
     }

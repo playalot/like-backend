223da63754b9823daa54692dd14ecae2cb2a39ac
Guan Guan <guanguan1114@gmail.com>
2015-09-22 21:21:00 +0800

    Remove files

--- like-core/src/main/scala/com/likeorz/dao/postgresql/PGEntityDAO.scala
+++
@@ -1,5 +0,0 @@
-package com.likeorz.dao.postgresql
-
-class PGEntityDAO {
-
-}
--- like-core/src/main/scala/com/likeorz/dao/postgresql/PGUserDAO.scala
+++
@@ -1,8 +0,0 @@
-package com.likeorz.dao.postgresql
-
-import play.api.Play.current
-import slick.driver.PostgresDriver.api._
-
-class PGUserDAO {
-
-}

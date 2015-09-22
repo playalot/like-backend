b0228e9b258850ce7246d0fca78c5644927eb1d4
Guan Guan <guanguan1114@gmail.com>
2015-09-22 21:16:31 +0800

    Add FutureUtils

--- like-core/src/main/scala/com/likeorz/utils/FutureUtils.scala
+++ like-core/src/main/scala/com/likeorz/utils/FutureUtils.scala
@@ -1,16 +1,17 @@
-package com.likeorz
+package com.likeorz.utils
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.Future
 import scala.util.{ Failure, Success }
+import play.api.libs.concurrent.Execution.Implicits._
 
-package object actors {
+object FutureUtils {
 
   private val tZero = System.currentTimeMillis()
 
   def printlnC(s: Any): Unit = println(Console.GREEN + s + Console.RESET)
   def printlnE(s: Any): Unit = println(Console.RED + s + Console.RESET)
 
-  def timedFuture[T](name: String)(f: Future[T])(implicit ec: ExecutionContext): Future[T] = {
+  def timedFuture[T](name: String)(f: Future[T]): Future[T] = {
     val start = System.currentTimeMillis()
     printlnC(s"--> started [$name] at t0 + ${start - tZero}")
     f.andThen {
@@ -21,6 +22,15 @@ package object actors {
         val end = System.currentTimeMillis()
         printlnE(s"\t<X> failed [$name], total time elapsed: ${end - start}\n$ex")
     }
+  }
+
+  def seqFutures[T, U](items: TraversableOnce[T])(func: T => Future[U]): Future[List[U]] = {
+    items.foldLeft(Future.successful[List[U]](Nil)) {
+      (f, item) =>
+        f.flatMap {
+          x => func(item).map(_ :: x)
+        }
+    } map (_.reverse)
   }
 
 }
